ed4e64d0b03f0b62984ed0606318c8e69fb94440
OMEN <grigorenkoeduard6@gmail.com>
2022-11-04 18:46:20 +0100

    Я понял как сделать только первую задачу вторую ума не приложу как решить

---
+++ src/osterixAndObelics.java
@@ -0,0 +1,11 @@
+public class osterixAndObelics {
+    public static void main(String[] args) {
+        int osterix = 3 ;
+        double obelix =1.5;
+        boolean potion = (obelix + osterix >= 5);
+        System.out.println(potion);
+
+
+        }
+    }
+
